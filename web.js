e957a7bffd1aba1dca4a65978f43439350256c0b
Ubuntu <ubuntu@ip-10-202-146-54.ec2.internal>
2013-07-11 23:45:42 +0000

    changed web.js to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html');
+var str = buf.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
